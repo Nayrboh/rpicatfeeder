2c45959e20ac66e1c66a6ac6212ca0695c6d47e1
Nayrboh <30458536+Nayrboh@users.noreply.github.com>
2017-07-25 23:55:12 -0600

    Add files via upload

---
+++ test.py
@@ -0,0 +1,17 @@
+import time 
+while True:
+    print("time for food")
+    import stepper2
+    time.sleep(3)   # Delay for 3 hours (10800 seconds).
+    print("time for food")
+    import stepper2
+    time.sleep(3)   # Delay for 3 hours (10800 seconds).
+    print("time for food")
+    import stepper2
+    time.sleep(3)   # Delay for 3 hours (10800 seconds).
+    print("time for food")
+    import stepper2
+    time.sleep(3)   # Delay for 3 hours (10800 seconds).
+    print("time for food")
+    import stepper2
+    time.sleep(12)   # Delay for 12 hours (10800 seconds).
